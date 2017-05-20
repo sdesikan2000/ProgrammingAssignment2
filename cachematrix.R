608807887d7c4edd8d3b75b2122bf854a21dcb76
sdesikan <sdesikan2000@gmail.com>
2017-05-20 20:08:15 +0800

    add existing file

--- cachematrix.R
+++ cachematrix.R
@@ -22,10 +22,9 @@ makeCacheMatrix <- function(x = matrix()) {
 ## This function computes the inverse of the special "matrix" returned by 
 ## makeCacheMatrix above. 
 ## If the inverse has already been calculated (and the matrix has not changed),
-## then the cachesolve should retrieve the inverse from the cache.
+## then the cacheSolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
        m <- x$getinverse()
         if(!is.null(m)) {
                 message("getting cached data")
